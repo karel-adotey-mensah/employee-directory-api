b4e61387ecf8b730c457776b5f485ed383531300
karel <kamens96@gmail.com>
2021-01-24 17:32:32 +0000

    search-and-filter-removed-from-api

--- routes/api.js
+++ routes/api.js
@@ -35,68 +35,6 @@ router.get("/employees", async (request, response) => {
 })
 
 /* -------------------------------------------------------------------------- */
-/*                         G E T  W I T H  F I L T E R                        */
-/* -------------------------------------------------------------------------- */
-router.post("/employees/query", async (request, response) => {
-    const shortQueryObject = await Employee.find(request.body,
-        {firstName: 1, lastName: 1, department:1, imageUrl:1})
-        try {
-            response.status(200).send({
-                success: true,
-                message: "Employee Data Found",
-                data: shortQueryObject
-            })
-        } catch (error) {
-            response.status(400).send({
-                success: false,
-                message: "Something Went Wrong",
-                error: error
-            })
-        }
-})
-/* -- Have To Use Post Method In Order To Send Data In Body Of Get Request -- */
-
-/* -------------------------------------------------------------------------- */
-/*                         G E T  W I T H  S E A R C H                        */
-/* -------------------------------------------------------------------------- */
-router.post("/employees/query/s", async (request, response) => {
-        const shortQueryObject = await Employee.find({},
-            {firstName: 1, lastName: 1, department:1, imageUrl:1})
-        const searchPhrase = request.body.searchPhrase.toLowerCase()
-        const isSearchItem = (firstName, lastName, department) => {
-            const fullName = firstName + " " + lastName
-            
-            return (
-                firstName.toLowerCase().includes(searchPhrase) ||
-                lastName.toLowerCase().includes(searchPhrase) ||
-                fullName.toLowerCase().includes(searchPhrase) ||
-                department.toLowerCase().includes(searchPhrase)
-                )
-        }
-        
-        try {
-            const searchResults =
-            shortQueryObject.filter(searchItem =>
-                isSearchItem(
-                    searchItem.firstName,
-                    searchItem.lastName,
-                    searchItem.department
-                    ))
-            response.status(200).send({
-                success: true,
-                message: "Employee Data Found",
-                data: searchResults,
-            })
-        } catch (error) {
-            response.status(400).send({
-                success: false,
-                message: "Something Went Wrong",
-                error: error
-            })
-        }
-})
-
-/* -------------------------------------------------------------------------- */
 /*                                   P O S T                                  */
 /* -------------------------------------------------------------------------- */
 router.post("/employees", async (request, response, next) => {
